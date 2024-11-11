831251107bdf58bfc39c8a6b785850cb22650612
Jesus Rodriguez <jarh1992@gmail.com>
2024-11-11 14:54:31 -0500

    fixed file

--- dojo/config/config_vars.py
+++ dojo/config/config_vars.py
@@ -1,21 +1,22 @@
 ranges = [
-    ('10k', 'undefined'),
-    ('9k', '9th kyu'),
-    ('8k', '8th kyu'),
-    ('7k', '7th kyu'),
-    ('6k', '6th kyu'),
-    ('5k', '5th kyu'),
-    ('4k', '4th kyu'),
-    ('3k', '3th kyu'),
-    ('2k', '2th kyu'),
-    ('1k', '1th kyu'),
-    ('1d', '1th Dan'),
-    ('2d', '2th Dan'),
-    ('3d', '3th Dan'),
-    ('4d', '4th Dan'),
-    ('5d', '5th Dan'),
-    ('6d', '6th Dan'),
-    ('7d', '7th Dan'),
-    ('8d', '8th Dan'),
-    ('9d', '9th Dan'),
+    ('10k', 'Beginner'),  # NA
+    ('9k', '9th kyu'),  # White
+    ('8k', '8th kyu'),  # Yellow
+    ('7k', '7th kyu'),  # Orange
+    ('6k', '6th kyu'),  # Green
+    ('5k', '5th kyu'),  # Violet
+    ('4k', '4th kyu'),  # Violet
+    ('3k', '3th kyu'),  # Brown
+    ('2k', '2th kyu'),  # Brown
+    ('1k', '1th kyu'),  # Brown
+    ('1d', '1th Dan'),  # Black
+    ('2d', '2th Dan'),  # Black
+    ('3d', '3th Dan'),  # Black
+    ('4d', '4th Dan'),  # Black
+    ('5d', '5th Dan'),  # Black
+    ('6d', '6th Dan'),  # Black
+    ('7d', '7th Dan'),  # Black
+    ('8d', '8th Dan'),  # Black
+    ('9d', '9th Dan'),  # Black
+    ('10d', '10th Dan'),  # Black
 ]
