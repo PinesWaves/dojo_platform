f2245b89a13fce7270f06612746e759591ce935d
Jesus Rodriguez <jarh1992@gmail.com>
2024-11-12 17:29:21 -0500

    added temp redirect home to login page

--- dojo/dojo/urls.py
+++ dojo/dojo/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from user_management.views import RegisterView
 
 urlpatterns = [
-    path('', lambda request: redirect('https://seishinjkadojo.com/')),
+    path('', lambda request: redirect('login/')),
     path('login/', LoginView.as_view(template_name='login_register/login.html'), name='login'),
     path('signup/', RegisterView.as_view(), name='signup'),
     path("attendance/", include('application.urls')),
