b32c523f0e7bc430dabda2bad63fa5c8b17bba33
Jesus Rodriguez <jarh1992@gmail.com>
2024-11-11 23:18:41 -0500

    added paths

--- dojo/dojo/urls.py
+++ dojo/dojo/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
 from django.urls import path, include
 
+from user_management.views import RegisterView
+
 urlpatterns = [
     path('', lambda request: redirect('https://seishinjkadojo.com/')),
-    path('login/', include('user_management.urls')),
+    path('login/', LoginView.as_view(template_name='login_register/login.html'), name='login'),
+    path('signup/', RegisterView.as_view(), name='signup'),
     path("attendance/", include('application.urls')),
     path('admin/', admin.site.urls),
 ]
