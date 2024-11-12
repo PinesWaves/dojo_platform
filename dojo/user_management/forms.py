d4c7acb7fbe64d3790f233b8c47644506e037967
Jesus Rodriguez <jarh1992@gmail.com>
2024-11-11 23:19:36 -0500

    added/updated user model/form

---
+++ dojo/user_management/forms.py
@@ -0,0 +1,94 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import User, Category
+
+
+class UserRegisterForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        required=True
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        required=True
+    )
+
+    accept_regulations = forms.BooleanField(
+        label="I accept the Regulations",
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'style': 'margin-left: 10px'}),
+        required=True
+    )
+
+    accept_inf_cons = forms.BooleanField(
+        label="I accept the Informed Consent",
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'style': 'margin-left: 10px'}),
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'id_type', 'id_number', 'birth_date', 'birth_place', 'profession', 'eps',
+            'phone_number', 'address', 'city', 'country', 'email', 'parent', 'parent_phone_number', 'category',
+            'password1', 'password2',
+        ]
+        exclude = ['accept_regulations', 'accept_inf_cons']
+
+    def __init__(self, *args, **kwargs):
+        # Extraemos el usuario actual del contexto si es pasado
+        self.request_user = kwargs.pop('request_user', None)
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = field.widget.attrs.get('class', '') + ' form-control'
+
+        # Si no es un usuario Sensei, deshabilitar el campo `categoria`
+        if not self.request_user or not self.request_user.is_sensei:
+            self.fields['category'].disabled = True
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords do not match.")
+        return password2
+
+    def clean_category(self):
+        category = self.cleaned_data['category']
+        # Verificar si el usuario actual tiene permiso para cambiar la categoría
+        if 'category' in self.changed_data and not self.request_user.is_sensei:
+            raise forms.ValidationError("Only users with category 'Sensei' can change the category.")
+        return category
+
+    def clean_accept_regulations(self):
+        accept = self.cleaned_data.get('accept_terms')
+        if not accept:
+            raise forms.ValidationError("You must accept the Regulations to continue.")
+        return accept
+
+    def clean_accept_inf_cons(self):
+        accept = self.cleaned_data.get('accept_terms')
+        if not accept:
+            raise forms.ValidationError("You must accept the Informed Consent to continue.")
+        return accept
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # Establecer la contraseña usando set_password para encriptarla
+        user.set_password(self.cleaned_data["password1"])
+        user.category = Category.ESTUDIANTE
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'id_type', 'id_number', 'birth_date', 'birth_place', 'profession', 'eps',
+            'phone_number', 'address', 'email', 'parent', 'parent_phone_number', 'category',
+        ]
--- dojo/user_management/models.py
+++ dojo/user_management/models.py
@@ -1,6 +1,42 @@
+import datetime
+
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from config.config_vars import ranges
+from django_countries.fields import CountryField
+
+
+class IDType(models.TextChoices):
+    CEDULA_CIUDADANIA = 'CC', 'Cédula de ciudadanía'
+    TARJETA_IDENTIDAD = 'TI', 'Tarjeta de Identidad'
+    CEDULA_EXTRANJERIA = 'CE', 'Cédula de extranjería'
+    PASAPORTE = 'PA', 'Pasaporte'
+    OTRO = 'OT', 'Otro'
+
+
+class Category(models.TextChoices):
+    SENSEI = 'S', 'Sensei'
+    ESTUDIANTE = 'E', 'Estudiante'
+
+
+class MedicalConditions(models.TextChoices):
+    ENFERMEDADES_CARDIACAS = 'EC', 'Enfermedades Cardiacas'
+    HIPERTENSION_ARTERIAL = 'HA', 'Hipertensión Arterial'
+    DIABETES = 'D', 'Diabetes'
+    ASMA_O_PROBLEMAS_RESPIRATORIOS = 'AoPR', 'Asma O Problemas Respiratorios'
+    EPILEPSIA_O_CONVULSIONES = 'EoC', 'Epilepsia O Convulsiones'
+    PROBLEMAS_MUSCULOESQUELETICOS = 'PME', 'Problemas Musculoesqueléticos (Ej. Esguinces, Fracturas)'
+    OTROS = 'OT', 'Otros'
+    NA = 'NA', 'No Aplica'
+
+
+class PhysicalConditions(models.TextChoices):
+    EXCELENTE = 'E', 'Excelente'
+    BUENO = 'B', 'Bueno'
+    ACEPTABLE = 'A', 'Aceptable'
+    NECESITA_MEJORAR = 'I', 'Necesita mejorar'
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -26,26 +62,64 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
-    email = models.EmailField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    id_type = models.CharField(
+        max_length=2,
+        choices=IDType.choices,
+        default=IDType.CEDULA_CIUDADANIA
+    )
+    category = models.CharField(
+        max_length=1,
+        choices=Category.choices,
+        default=Category.ESTUDIANTE
+    )
+    id_number = models.CharField(max_length=30, unique=True, null=False, blank=False, default=0)
+    birth_date = models.DateField(default=datetime.datetime(2000, 1, 1))
+    birth_place = models.CharField(max_length=30, default='')
+    profession = models.CharField(max_length=30, default='')
+    eps = models.CharField(max_length=50, default='')
+    phone_number = models.CharField(max_length=15, default='')
+    address = models.CharField(max_length=255, default='')
+    city = models.CharField(max_length=30, default='')
+    country = models.CharField(max_length=30, default='')
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    level = models.CharField(choices=ranges, default=ranges[0])
+    parent = models.CharField(max_length=60, null=True, blank=True, default='')
+    parent_phone_number = models.CharField(max_length=15, null=True, blank=True, default='')
+    accept_inf_cons = models.BooleanField(default=False)
+    medical_cond = models.CharField(choices=MedicalConditions.choices, default=MedicalConditions.NA)
+    drug_cons = models.CharField(max_length=60, null=True, blank=True, default='')
+    allergies = models.CharField(max_length=60, null=True, blank=True, default='')
+    other_activities = models.CharField(max_length=60, null=True, blank=True, default='')
+    cardio_prob = models.BooleanField(default=False)
+    injuries = models.BooleanField(default=False)
+    physical_limit = models.BooleanField(default=False)
+    # Lost consciousness or lost balance after feeling dizzy
+    lost_cons = models.BooleanField('lost consciousness', default=False)
+    physical_cond = models.CharField(choices=PhysicalConditions.choices, default=PhysicalConditions.ACEPTABLE)
+    # Follow the instructor's recommendations and safety rules during the classes.
+    sec_recom = models.BooleanField(default=False)
+    # I have read, understand the questions, completed and answered the questionnaire with my acceptance?
+    agreement = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     date_deactivated = models.DateTimeField(null=True)
     date_reactivated = models.DateTimeField(null=True)
-    level = models.CharField(choices=ranges, default=ranges[0])
-    address = models.CharField(max_length=255, default='')
-    phone_number = models.CharField(max_length=15, default='')
     payment = models.IntegerField(default=0)
     payment_status = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'id_number'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     objects = UserManager()
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    @property
+    def is_sensei(self):
+        return self.category == Category.SENSEI
