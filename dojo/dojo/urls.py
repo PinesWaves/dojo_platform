3f30a3b28d2f501fdc8fa9ccaca5d330299c70fc
Jesus Rodriguez <jarh1992@gmail.com>
2024-11-25 00:17:43 -0500

    edited url to use with tokens

--- dojo/dojo/urls.py
+++ dojo/dojo/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('', lambda request: redirect('login/')),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('signup/', RegisterView.as_view(), name='signup'),
-    path("dashboard/", include('dashboard.urls')),
+    path('signup/<str:token>/', RegisterView.as_view(), name='signup'),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
 
