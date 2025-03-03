3a0cf7c0d85f867266e0cc82adf5e8326bbd50ff
Jesus Rodriguez <jarh1992@gmail.com>
2025-03-02 23:54:11 -0500

    added Dojo to admin

--- dojo/dashboard/admin.py
+++ dojo/dashboard/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Dojo
 
-# Register your models here.
+@admin.register(Dojo)
+class DojoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'sensei', 'description')  # Customize columns
+    search_fields = ('name', 'image', 'description')  # Add search fields
+    # list_filter = ('name', 'sensei')  # Add filters
+    ordering = ('name',)  # Default ordering
+    fieldsets = (
+        (None, {'fields': ('name', 'sensei')}),
+        ('Info', {'fields': ('image', 'description', 'students')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'image', 'sensei', 'description'),
+        }),
+    )
