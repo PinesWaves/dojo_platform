92e8b7def65ffab60df254b860e1058babd0c919
Jesus Rodriguez <jarh1992@gmail.com>
2025-03-27 21:36:11 -0500

    edited choices

--- dojo/user_management/models.py
+++ dojo/user_management/models.py
@@ -13,31 +13,31 @@ from config.config_vars import Ranges
 
 class IDType(models.TextChoices):
     CEDULA_CIUDADANIA = 'CC', 'Citizenship card'
-    TARJETA_IDENTIDAD = 'TI', 'Identity Card'
-    CEDULA_EXTRANJERIA = 'CE', 'Alien Registration Card'
+    TARJETA_IDENTIDAD = 'IC', 'Identity Card'
+    CEDULA_EXTRANJERIA = 'ARC', 'Alien Registration Card'
     PASAPORTE = 'PA', 'Passport'
     OTRO = 'OT', 'Otro'
 
 
 class Category(models.TextChoices):
-    SENSEI = 'S', 'Sensei'
-    ESTUDIANTE = 'E', 'Student'
+    SENSEI = 'SE', 'Sensei'
+    ESTUDIANTE = 'ST', 'Student'
 
 
 class MedicalConditions(models.TextChoices):
-    ENFERMEDADES_CARDIACAS = 'EC', 'Cardiac Diseases'
-    HIPERTENSION_ARTERIAL = 'HA', 'Arterial Hypertension'
+    ENFERMEDADES_CARDIACAS = 'CD', 'Cardiac Diseases'
+    HIPERTENSION_ARTERIAL = 'AH', 'Arterial Hypertension'
     DIABETES = 'D', 'Diabetes'
-    ASMA_O_PROBLEMAS_RESPIRATORIOS = 'AoPR', 'Asthma and respiratory problems'
-    EPILEPSIA_O_CONVULSIONES = 'EoC', 'Epilepsy or Seizures'
-    PROBLEMAS_MUSCULOESQUELETICOS = 'PME', 'Musculoskeletal Problems (e.g. Sprains, Fractures)'
+    ASMA_O_PROBLEMAS_RESPIRATORIOS = 'ARP', 'Asthma and respiratory problems'
+    EPILEPSIA_O_CONVULSIONES = 'ES', 'Epilepsy or Seizures'
+    PROBLEMAS_MUSCULOESQUELETICOS = 'MP', 'Musculoskeletal Problems (e.g. Sprains, Fractures)'
     OTROS = 'OT', 'Others'
     NA = 'NA', 'No aplicable'
 
 
 class PhysicalConditions(models.TextChoices):
     EXCELENTE = 'E', 'Excellent'
-    BUENO = 'B', 'Good'
+    BUENO = 'G', 'Good'
     ACEPTABLE = 'A', 'Acceptable'
     NECESITA_MEJORAR = 'I', 'Needs improvement'
 
