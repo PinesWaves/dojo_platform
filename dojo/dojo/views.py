0fa51d840c80c4dee9b59f6b6246a2a8561880c7
Jesus Rodriguez <jarh1992@gmail.com>
2025-05-25 00:17:13 -0500

    added landing context

--- dojo/dojo/views.py
+++ dojo/dojo/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 
 def landing_page(request):
-    return render(request, 'landing/landing.html')
+    ctx = {
+        'galery': [static(f'img/galery/{x:02}.png') for x in range(1, 16)],
+            # 'https://dummyimage.com/600x400/000/fff',
+            # 'https://dummyimage.com/600x400/111/fff',
+            # 'https://dummyimage.com/600x400/222/fff',
+            # 'https://dummyimage.com/600x400/333/fff',
+            # 'https://dummyimage.com/600x400/444/fff',
+            # 'https://dummyimage.com/600x400/555/fff',
+            # 'https://dummyimage.com/600x400/666/fff',
+    }
+    return render(request, 'landing/landing.html', ctx)
