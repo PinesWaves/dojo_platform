ac538ddcdd6edd100323deaf00b853f56e9f6a8f
Jesus Rodriguez <jarh1992@gmail.com>
2025-06-05 23:14:53 -0500

    commented unused urls

--- dojo/user_management/urls.py
+++ dojo/user_management/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from django.contrib.auth.views import LoginView
-from .views import RegisterView, UpdateUserView
+# from django.urls import path
+# from django.contrib.auth.views import LoginView
+# from .views import RegisterView, UpdateUserView
 
-urlpatterns = [
+# urlpatterns = [
     # path('update/<int:pk>/', UpdateUserView.as_view(), name='update_user'),
-]
+# ]
