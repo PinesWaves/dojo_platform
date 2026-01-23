e10dbf8604e4cb2ca363f25c942a4bb1e6c72ed0
Jesus Rodriguez <jarh1992@gmail.com>
2026-01-22 22:26:22 -0500

    fixed serving media files

--- dojo/dojo/urls.py
+++ dojo/dojo/urls.py
@@ -43,10 +43,10 @@ if settings.DEBUG:
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+# Always serve media files (Railway doesn't use nginx)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 handler404 = 'dojo.views.custom_404'
 handler500 = 'dojo.views.custom_500'
